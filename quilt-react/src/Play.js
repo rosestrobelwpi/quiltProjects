1c3f1b9f1444a6090256a95fdbbfe0302525c473
Rose Strobel <97707284+rosestrobelwpi@users.noreply.github.com>
2024-11-07 05:15:58 -0500

    Update Play.js

--- quilt-react/src/Play.js
+++ quilt-react/src/Play.js
@@ -1,160 +1,110 @@
-import React, { useState, useRef, useEffect } from "react";
-import styles from './App.css';
-import {UnControlled as CodeMirror} from 'react-codemirror2'
-import "codemirror/lib/codemirror.css";
-import "codemirror/theme/material.css";
-import "codemirror/mode/javascript/javascript";
-import codemirror from "codemirror";
-import { Link } from 'react-router-dom';
-import { Alert } from "bootstrap";
-// import Parser from './autogenparser';
-// import { Patch, Design, evaluator } from './interpreter';
-// import ASTFunction from './parserASTfunction';
-
-function Play() {
-
-  const [value, setTextareaValue] = useState("");
-  const canvasRef = useRef(null);
-
-  const handleClear = () => {
-    setTextareaValue("");
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-  };
-
-  // const visualizer = () => {
-  //   const canvas = canvasRef.current;
-  //   const ctx = canvas.getContext("2d");
-
-  //   if (evaluator instanceof Patch) {
-  //     ctx.fillStyle = evaluator.color;
-  //     ctx.fillRect(evaluator.x, evaluator.y, evaluator.w, evaluator.h);
-  //   }
-  //   else if (evaluator instanceof Design) {
-  //     evaluator.patches.forEach(patch => {
-  //       visualizer(ctx, patch)
-  //     });
-  //   }
-  // }
-
-  // const codeToCanvas = (text) => {
-  //   // const parser = Parser.parse(value);
-  //   // const interpreter = evaluator({}, ASTFunction)
-
-  //   const canvas = canvasRef.current;
-  //   const ctx = canvas.getContext("2D");
-  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  //   // visualizer(ctx, interpreter);
-  // }
-
-  const codeToCanvas = (value) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "16px";
-    ctx.fillText(value, 100, 100);
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    font-family: Arial, sans-serif;
+    overflow-y: auto;
   }
 
-  const handleCodeChange = (editor, data, value) => {
-    setTextareaValue(value);
-    codeToCanvas(value);
-  };
+  .about-container {
+    background-color: #2a2a2e;
+    color: #d9d9e0;
+  }
 
-  const handleSubmit = () => {
-    console.log("Text value: ", value);
-    window.alert("You entered: " + value)
-  };
-
-  useEffect(() => {
-    const keyPressed = (event) => {
-      if (event.shiftKey && event.key === "Enter") {
-        handleSubmit();
-        event.preventDefault();
-      }
-    };
-    window.addEventListener("keydown", keyPressed);
-
-    return() => {
-      window.removeEventListener("keydown", keyPressed);
-    };
-  }, [value]);
-
-
-
-  return (
-    <div className="play-container">
-    <div className="navbar">
-      <ul>
-        <li>
-          <a href="/" id="logo">Quilt Designer</a>
-        </li>
-      </ul>
-      <div className="navbar-links">
-        <ul>
-          <li>
-            <a href="/">Home</a>    
-          </li>
-          <li>
-            <a href="/examples">Docs</a>    
-          </li>
-          <li>
-            <a href="/about">About Us</a>
-          </li>
-        </ul>
-      </div>
-      </div>
-      {/* <div className="play-header">
-        <h1>Quilt Designer</h1>
-       
-      </div> */}
-{/*     
-      <div className="button-container">
-      <button id="home-button"><a href="/">Home</a></button>
-      <button id="example-button"><a href="/">Examples</a></button>
-        <h1>Quilt Designer</h1>
-        
-        <button id="submit-button" onClick={handleSubmit}>Submit</button>
-        <button id="clear-button" onClick={handleClear}>Clear</button>
-      </div> */}
-      
-      <div className="container2">
-
-      <div className="button-help">
-        <div className="btn-action">
-          <code>Shift + Enter</code> <span>to submit</span>
-        </div>
-        <div className="btn-action">
-          <code>Ctrl + Backspace</code> <span>to clear</span>
-        </div>
-      </div>  
-
-
-        <div className="parser-container">
-          <CodeMirror
-            value=''
-            options={{
-              mode: 'javascript',
-              theme: 'material',
-              lineNumbers: true
-            }}
-            onChange={(editor, data, value) => {
-              setTextareaValue(value); 
-              codeToCanvas(value);
-                }}
-            onInputRead={(editor, canvasRef) => {
-            }}
-              />
-          
-        </div>
-        <div className="drawing-container">
-          <canvas id="canvas" ref={canvasRef}></canvas>
-        </div>
-      </div>
-    </div>
-  );
+  .navbar {
+    background-color: #414146;
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 5em;
+    display: flex;
+    align-items: center;
+    justify-content: space-between; 
 }
+  
+  .navbar-links {
+    display: flex;
+    justify-content: flex-end;
+  }
 
-export default Play;
+  .navbar ul {
+    display: flex;
+    gap: 5em;
+    list-style: none;
+    margin-right: 2em;
+  }
+  
+  .navbar a {
+    text-decoration: none;
+    color: #ffffff;
+  }
+
+  #logo {
+    margin-left: 2em;
+    font-weight: 700;
+  }
+
+  .about-top-container {
+    text-align: center;
+    margin-top: 10em;
+  }
+
+  .about-top-container h1 {
+    font-size: 45px;
+    max-width: 35%;
+    margin: 0 auto;
+    margin-bottom: 20px;
+  }
+
+  .about-top-container p {
+    font-size: 20px;
+    margin: 0 auto;
+    line-height: 1.5;
+    max-width: 30%;
+    margin-bottom: 5em;
+  }
+
+
+  .about-text-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    padding: 2em;
+    gap: 2em;
+  }
+
+  .about-text-content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: larger;
+    gap: 20px;
+  }
+
+  .about-text-content p {
+    width: 70%;
+    line-height: 1.5;
+  }
+
+
+  .about-img-container {
+    flex: 1;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+
+  #img2 {
+    flex: 1;
+    max-width: 40vw;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
