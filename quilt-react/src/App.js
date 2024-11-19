c4e69edd9edf36cbef9ec6c50abc9e90905f0334
TheCoolBT <120990100+TheCoolBT@users.noreply.github.com>
2024-11-19 15:47:09 -0500

    Added error messages

--- quilt-react/src/App.js
+++ quilt-react/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Play from './Play';
-import Home from './Home'
-import About from './About'
-import Examples from './Examples'
-import GettingStarted from './GettingStarted'
-import ExampleOne from './examples/ExampleOne'
-
+import Home from './Home';
+import About from './About';
+import Examples from './Examples';
+import GettingStarted from './GettingStarted';
+import ExampleOne from './examples/ExampleOne';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/play" element={<Play />} />
+      {/* Add a dynamic route to handle preloaded designs */}
+      <Route path="/play/:code" element={<Play />} />
       <Route path="/about" element={<About />} />
       <Route path="/examples" element={<Examples />} />
       <Route path="/exampleOne" element={<ExampleOne />} />
