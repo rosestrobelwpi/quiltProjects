4f487ac914b8ed921c6ae598b260f58bc2534eb9
TheCoolBT <120990100+TheCoolBT@users.noreply.github.com>
2025-01-30 16:46:43 -0500

    tested if deploy worked

--- quilt-react/src/Play.js
+++ quilt-react/src/Play.js
@@ -19,7 +19,7 @@ import imageSRC from './laura_test_image/larry.png';
 const colorPalette = {
     red: '#b57c7c',
     orange: '#d9a078',
-    yellow: '#c8b77a',
+    yellow: '#b57c7c',
     green: '#85a586',
     blue: '#6a8caf',
     purple: '#9e86a6',
